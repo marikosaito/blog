6806dff493abdcbe06668befc02d3664d3565007
marikosaito <marikovalle@icloud.com>
2018-12-12 14:48:42 +0900

    New

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :show, :destroy, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to action: :index
+  end
+
+  def edit
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to action: :index
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to action: :index
+  end
+
+  def show
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:text)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+    end
+
+
+end
